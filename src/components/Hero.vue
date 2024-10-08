b982d87e81479b0bd6bbf2b4507e03be30b9c5fd
MarkMallari16 <mallariftw01@gmail.com>
2024-10-08 11:50:29 +0800

    add hero-section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,25 @@
-<script setup></script>
+<script setup>
+import HeroImage from "../assets/hero-image.png";
+</script>
 <template>
-  <div class="min-h-screen grid place-content-center">
-    <h1 class="text-5xl lg:text-6xl text-center">Basic Hero Section</h1>
-    <div class="mt-4 flex justify-center gap-2 ">
-      <router-link to="/signup" class="font-medium btn btn-primary"
-        >Get started</router-link
-      >
-      <router-link to="/login" class="font-medium btn btn-ghost">Log in</router-link>
+  <div class="mx-8 min-h-screen flex justify-center items-center gap-10">
+    <div class="text-center">
+      <h1 class="text-5xl lg:text-7xl font-black">Track Your Budget Effortlessly!</h1>
+      <p class="mt-3">
+        Take Control of Your Finances" or "Track Your Budget Effortlessly
+      </p>
+      <div class="mt-4 flex justify-center gap-2 ">
+        <router-link to="/signup" class="font-medium btn btn-primary"
+          >Get Started for Free</router-link
+        >
+        <router-link to="/login" class="font-medium btn btn-ghost"
+          >Learn More</router-link
+        >
+      </div>
+
+      <div class="mt-8 w-full  hidden lg:flex justify-center">
+        <img :src="HeroImage" alt="Budget Tracker Image" class="w-4/6 object-cover rounded-sm shadow-lg" />
+      </div>
     </div>
   </div>
 </template>
