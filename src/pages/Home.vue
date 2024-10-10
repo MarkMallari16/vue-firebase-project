7815b393cb2887f34f3d676e9e3e6f56ed98f123
MarkMallari16 <mallariftw01@gmail.com>
2024-10-10 21:18:23 +0800

    add icon in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -88,13 +88,12 @@ const handleSignOut = () => {
               className="dropdown-content menu bg-white rounded-box z-[1] w-32 overflow-hidden p-2 shadow "
             >
               <li><button @click="handleSignOut">Logout</button></li>
-             
             </ul>
           </div>
         </div>
 
         <div class="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
-          <div class="rounded-sm p-10 ring-1 ring-inset ring-base-300 bg-white">
+          <div class="rounded-sm p-8 ring-1 ring-inset ring-base-300 bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -112,8 +111,25 @@ const handleSignOut = () => {
             </svg>
 
             <h2 class="text-2xl">Income: <span class="font-bold">20040</span></h2>
+
+            <div class="flex items-center gap-2 pt-4 text-green-500 font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
+            </div>
           </div>
-          <div class="rounded-sm p-10 ring-1 ring-inset ring-base-300 bg-white">
+          <div class="rounded-sm p-8 ring-1 ring-inset ring-base-300 bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -132,8 +148,25 @@ const handleSignOut = () => {
             </svg>
 
             <h2 class="text-2xl">Expenses: <span class="font-bold">2000</span></h2>
+
+            <div class="flex items-center gap-2 pt-4 text-green-500 font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
+            </div>
           </div>
-          <div class="rounded-sm p-10 ring-1 ring-inset ring-base-300 bg-white">
+          <div class="rounded-sm p-8 ring-1 ring-inset ring-base-300 bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -153,6 +186,23 @@ const handleSignOut = () => {
             <h2 class="text-2xl">
               Remaining Budget: <span class="font-bold">2000</span>
             </h2>
+
+            <div class="flex items-center gap-2 pt-4 text-red-500 font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+
+              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
+            </div>
           </div>
         </div>
       </div>
