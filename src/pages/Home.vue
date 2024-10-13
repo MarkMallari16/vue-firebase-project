af355f6dfd5f34a84863bcc84d36c14f102b19a7
MarkMallari16 <mallariftw01@gmail.com>
2024-10-13 23:14:49 +0800

    add icons in sidebar

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -137,8 +137,9 @@ const handleSignOut = () => {
         class="bg-white w-full px-5 py-8 rounded-lg ring-1 ring-inset ring-base-300 h-full"
       >
         <div>
+          <!--Dashboard-->
           <div
-            class="flex gap-4 items-center px-2 py-3 bg-base-300 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+            class="flex gap-4 items-center px-4 py-3 bg-base-300 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -158,37 +159,30 @@ const handleSignOut = () => {
             <h2 class="font-medium">Dashboard</h2>
           </div>
 
+          <!--Transactions-->
           <div
-            class="flex gap-4 items-center px-2 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+            class="flex gap-4 items-center px-4 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
           >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h18v18H3zM8 7h8v2H8zM8 11h8v2H8zM8 15h8v2H8z" />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 3h18v18H3zM8 7h8v2H8zM8 11h8v2H8zM8 15h8v2H8z"
+              />
+            </svg>
 
             <h2 class="font-medium">Transaction</h2>
           </div>
 
           <div
-            class="flex gap-4 items-center px-2 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 6h15M3 12h18M4.5 18h15" />
-          </svg>
-            <h2 class="font-medium">Budget Overview</h2>
-          </div>
-
-          <div
-            class="flex gap-4 items-center px-2 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 6h15M3 12h18M4.5 18h15" />
-          </svg>
-
-            <h2 class="font-medium">Categories</h2>
-          </div>
-
-          <div
-            class="flex gap-4 items-center px-2 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+            class="flex gap-4 items-center px-4 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -201,11 +195,59 @@ const handleSignOut = () => {
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                d="M4.5 6h15M3 12h18M4.5 18h15"
+              />
+            </svg>
+            <h2 class="font-medium">Budget Overview</h2>
+          </div>
+
+          <div
+            class="flex gap-4 items-center mt-8 px-4 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 6h15M3 12h18M4.5 18h15"
               />
             </svg>
 
-            <h2 class="font-medium">Dashboard</h2>
+            <h2 class="font-medium">Categories</h2>
+          </div>
+
+          <div
+            class="flex gap-4 items-center px-4 py-3 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12M6 12h12" />
+            </svg>
+            <h2 class="font-medium">Goals</h2>
+          </div>
+
+          <!-- Settings -->
+          <div
+            class="flex gap-4 items-center px-4 py-3 mt-10 hover:bg-base-300 rounded-lg cursor-pointer mb-3"
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+          
+            <h2 class="font-medium">Settings</h2>
           </div>
         </div>
       </div>
