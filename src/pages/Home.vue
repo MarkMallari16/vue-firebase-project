c215d4eadf916fc1b711897db97ad12544133719
MarkMallari16 <mallariftw01@gmail.com>
2024-10-13 22:59:44 +0800

    fix some ui

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -39,7 +39,89 @@ const handleSignOut = () => {
 
 <template>
   <div class="min-h-screen pt-2 bg-base-200">
-    <div class="grid grid-cols-[1fr_4fr] gap-4">
+    <!--Top-->
+    <div
+      class="flex flex-col lg:flex-row justify-between items-center gap-10 bg-white px-4 ring-1 ring-inset ring-gray-300 py-4 mb-2 rounded-lg"
+    >
+      <div>
+        <h1 class="text-3xl font-bold uppercase">Budget Tracker</h1>
+      </div>
+      <div class="w-1/2 relative">
+        <input
+          type="text"
+          class="input input-bordered pl-12 w-full"
+          placeholder="Search..."
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-5 absolute left-4 top-4"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+
+      <div class="flex items-center gap-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6 text-primary"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+          />
+        </svg>
+        <button @click="handleSignOut">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </button>
+        <!-- <div class="dropdown">
+         <div tabIndex="{0}" role="button" class="btn m-1 bg-white">
+           <svg
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24"
+             fill="currentColor"
+             class="size-6"
+           >
+             <path
+               fill-rule="evenodd"
+               d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+               clip-rule="evenodd"
+             />
+           </svg>
+         </div>
+         <ul
+           tabIndex="{0}"
+           class="dropdown-content menu bg-white rounded-box z-[1] w-32 overflow-hidden p-2 shadow"
+         >
+           <li><button @click="handleSignOut">Logout</button></li>
+         </ul>
+       </div>-->
+      </div>
+    </div>
+    <div class="grid grid-cols-[1fr_4fr] gap-3">
       <!--sidebar-->
       <div class="bg-white w-full p-5 rounded-lg ring-1 ring-inset ring-base-300">
         <div class="mb-6">
@@ -53,78 +135,16 @@ const handleSignOut = () => {
               />
             </div>
           </div>
-         <div>
-          <h3 class="font-medium text-md">{{user.email}}</h3>
-          <span class="text-xs text-green-500 font-medium">Online</span>
-         </div>
+          <div>
+            <h3 class="font-medium text-md">{{ user.email }}</h3>
+            <span class="text-xs text-green-500 font-medium">Online</span>
+          </div>
         </div>
       </div>
       <!--content-->
       <div class="w-full pe-2">
-        <!--Top-->
-        <div
-          class="flex flex-col lg:flex-row justify-between items-center gap-10 bg-white px-6 py-4 rounded-lg"
-        >
-          <div class="w-full relative">
-            <input
-              type="text"
-              class="input input-bordered pl-12 w-full"
-              placeholder="Search..."
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-5 absolute left-4 top-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-
-          <div class="flex items-center gap-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-6"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-
-           <!-- <div class="dropdown">
-              <div tabIndex="{0}" role="button" class="btn m-1 bg-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="size-6"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex="{0}"
-                class="dropdown-content menu bg-white rounded-box z-[1] w-32 overflow-hidden p-2 shadow"
-              >
-                <li><button @click="handleSignOut">Logout</button></li>
-              </ul>
-            </div>-->
-          </div>
-        </div>
         <!--Overview-->
-        <div class="mt-4 grid grid-cols-1 lg:grid-cols-4 gap-5">
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-5">
           <div class="rounded-lg p-8 ring-1 ring-inset ring-base-300 bg-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -284,7 +304,7 @@ const handleSignOut = () => {
           <div
             class="mt-4 bg-white p-10 w-full rounded-lg ring-1 ring-inset ring-base-300"
           >
-            <h2 class="text-xl  font-bold">Budget Spending History</h2>
+            <h2 class="text-xl font-bold">Budget Spending History</h2>
             <DashboardLineChart />
           </div>
         </div>
