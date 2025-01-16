732cf20640b764f2b0247baaa481bda623c0a640
MarkMallari16 <mallariftw01@gmail.com>
2025-01-16 11:46:47 +0800

    cleaning structure'

---
+++ src/components/DashboardChart.vue
@@ -0,0 +1,17 @@
+<script setup>
+import DashboardBarChart from "./DashboardBarChart.vue";
+import DashboardLineChart from "./DashboardLineChart.vue";
+</script>
+
+<template>
+  <div class="flex flex-wrap lg:flex-nowrap gap-5">
+    <div class="mt-4 bg-white p-10 w-full rounded-md ring-1 ring-inset ring-base-300">
+      <h2 class="text-xl font-bold">History</h2>
+      <DashboardBarChart />
+    </div>
+    <div class="mt-4 bg-white p-10 w-full rounded-md ring-1 ring-inset ring-base-300">
+      <h2 class="text-xl font-bold">Budget Spending History</h2>
+      <DashboardLineChart />
+    </div>
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,8 @@ import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import DashboardBarChart from "@/components/DashboardBarChart.vue";
 import DashboardLineChart from "@/components/DashboardLineChart.vue";
+import DashboardOverview from "@/components/DashboardOverview.vue";
+import DashboardChart from "@/components/DashboardChart.vue";
 
 const router = useRouter();
 const isLoggedIn = ref(false);
@@ -116,7 +118,6 @@ const handleSignOut = () => {
         class="bg-white w-full px-5 py-8 rounded-md ring-1 ring-inset ring-base-300 h-full"
       >
         <div>
-          <!--Dashboard-->
           <div
             class="flex gap-4 items-center px-4 py-3 bg-base-300 hover:bg-base-300 rounded-md cursor-pointer mb-3"
           >
@@ -250,175 +251,12 @@ const handleSignOut = () => {
           </div>
         </div>
       </div>
-      <!--content-->
+
+      <!--Overview-->
       <div class="w-full">
-        <!--Overview-->
-        <div class="grid grid-cols-1 lg:grid-cols-4 gap-5">
-          <div class="rounded-md p-8 ring-1 ring-inset ring-base-300 bg-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-10"
-            >
-              <path
-                d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-
-            <h2 class="mt-2 text-lg uppercase text-gray-700">Income</h2>
-            <h1 class="text-4xl font-bold">20040</h1>
-
-            <div class="flex items-center gap-2 pt-4 text-green-500 font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="size-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-
-              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
-            </div>
-          </div>
-
-          <div class="rounded-md p-8 ring-1 ring-inset ring-base-300 bg-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-10"
-            >
-              <path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
-              <path
-                fill-rule="evenodd"
-                d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
-                clip-rule="evenodd"
-              />
-              <path
-                d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z"
-              />
-            </svg>
-
-            <h2 class="mt-2 text-lg uppercase text-gray-700">Expense</h2>
-            <h1 class="text-4xl font-bold">20040</h1>
-
-            <div class="flex items-center gap-2 pt-4 text-green-500 font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="size-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-
-              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
-            </div>
-          </div>
-
-          <div class="rounded-md p-8 ring-1 ring-inset ring-base-300 bg-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-10"
-            >
-              <path
-                d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 0 0 7.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 0 0 4.902-5.652l-1.3-1.299a1.875 1.875 0 0 0-1.325-.549H5.223Z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 0 0 9.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 0 0 2.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3Zm3-6a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75v-3Zm8.25-.75a.75.75 0 0 0-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-5.25a.75.75 0 0 0-.75-.75h-3Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-
-            <h2 class="mt-2 text-lg uppercase text-gray-700">Remaining budget</h2>
-            <h1 class="text-4xl font-bold">20040</h1>
-
-            <div class="flex items-center gap-2 pt-4 text-red-500 font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="size-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-
-              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
-            </div>
-          </div>
-
-          <div class="rounded-md p-8 ring-1 ring-inset ring-base-300 bg-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-10"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                clip-rule="evenodd"
-              />
-            </svg>
-
-            <h2 class="mt-2 text-lg uppercase text-gray-700">Total Savings</h2>
-            <h1 class="text-4xl font-bold">20040</h1>
-
-            <div class="flex items-center gap-2 pt-4 text-green-500 font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="size-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M15.22 6.268a.75.75 0 0 1 .968-.431l5.942 2.28a.75.75 0 0 1 .431.97l-2.28 5.94a.75.75 0 1 1-1.4-.537l1.63-4.251-1.086.484a11.2 11.2 0 0 0-5.45 5.173.75.75 0 0 1-1.199.19L9 12.312l-6.22 6.22a.75.75 0 0 1-1.06-1.061l6.75-6.75a.75.75 0 0 1 1.06 0l3.606 3.606a12.695 12.695 0 0 1 5.68-4.974l1.086-.483-4.251-1.632a.75.75 0 0 1-.432-.97Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-
-              <p>+2.5% <span class="text-gray-600 font-normal">last month</span></p>
-            </div>
-          </div>
-        </div>
+        <DashboardOverview />
         <!--Chart-->
-        <div class="flex flex-wrap lg:flex-nowrap gap-5">
-          <div
-            class="mt-4 bg-white p-10 w-full rounded-md ring-1 ring-inset ring-base-300"
-          >
-            <h2 class="text-xl font-bold">History</h2>
-            <DashboardBarChart />
-          </div>
-          <div
-            class="mt-4 bg-white p-10 w-full rounded-md ring-1 ring-inset ring-base-300"
-          >
-            <h2 class="text-xl font-bold">Budget Spending History</h2>
-            <DashboardLineChart />
-          </div>
-        </div>
+        <DashboardChart />
       </div>
     </div>
   </div>
