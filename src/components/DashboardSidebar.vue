7ad6d7237245b59f80f5fd1e8147b7d1990fa556
MarkMallari16 <mallariftw01@gmail.com>
2025-01-23 09:16:20 +0800

    add fill active

--- src/components/DashboardSidebar.vue
+++ src/components/DashboardSidebar.vue
@@ -30,11 +30,26 @@ const isActive = (path) => route.path === path;
           stroke-width="1.5"
           stroke="currentColor"
           class="size-6"
+          v-if="!isActive('/home')"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6"
+          v-else
+        >
+          <path
+            d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"
+          />
+          <path
+            d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"
           />
         </svg>
 
@@ -57,6 +72,7 @@ const isActive = (path) => route.path === path;
           stroke-width="1.5"
           stroke="currentColor"
           class="size-6"
+          v-if="!isActive('/transactions')"
         >
           <path
             stroke-linecap="round"
@@ -65,6 +81,20 @@ const isActive = (path) => route.path === path;
           />
         </svg>
 
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-6"
+          v-else
+        >
+          <path
+            d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z"
+          />
+          <path
+            d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z"
+          />
+        </svg>
         <h2 class="font-medium">Transaction</h2>
       </button>
 
