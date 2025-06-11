40e9a1016dc204ba47a124a64d2626e4472a56b2
Mark Christian Mallari <mallariftw01@gmail.com>
2025-06-11 22:17:52 +0800

    add: mockup motion animation

--- src/pages/Guest.vue
+++ src/pages/Guest.vue
@@ -7,6 +7,7 @@ import Footer from "@/components/Footer.vue";
 import Features from "@/components/Features.vue";
 import SocialProof from "@/components/SocialProof.vue";
 import { ref } from "vue";
+import { delay, motion, useScroll } from "motion-v";
 
 const features = ref(null);
 const pricing = ref(null);
@@ -23,20 +24,45 @@ const scrollToSection = (section) => {
     target.scrollIntoView({ behavior: "smooth" });
   }
 };
+
+const { scrollYProgress } = useScroll();
+const scrollIndicator = {
+  scaleX: scrollYProgress,
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  height: "10px",
+  originX: 0,
+  backgroundColor: "#000000",
+};
 </script>
 <template>
   <section class="relative overflow-hidden">
+    <motion.div
+      class="origin-left bg-primary fixed top-0 right-0 left-0 h-3"
+      id="scrollIndicator"
+      :style="scrollIndicator"
+    />
     <div>
       <div>
         <div>
           <Navbar @scrollTo="scrollToSection" />
         </div>
-        <div>
+        <motion.div
+          :initial="{ x: -50, opacity: 0, delay: 2 }"
+          :whileInView="{ x: 0, opacity: 1, delay: 2 }"
+        >
           <Hero />
-        </div>
-        <div ref="features" class="container mx-auto px-4">
+        </motion.div>
+        <motion.div
+          :initial="{ y: -50, opacity: 0, delay: 2 }"
+          :whileInView="{ y: 0, opacity: 1, delay: 2 }"
+          ref="features"
+          class="container mx-auto px-4"
+        >
           <Features />
-        </div>
+        </motion.div>
         <div>
           <Bar />
         </div>
@@ -50,7 +76,11 @@ const scrollToSection = (section) => {
       <Footer />
     </div>
     <!-- Decorative SVG -->
-    <div class="absolute top-[12%] -left-32 hidden lg:block">
+    <motion.div
+      :initial="{ y: -50, opacity: 0, delay: 2 }"
+      :whileInView="{ y: 0, opacity: 1, delay: 2 }"
+      class="absolute top-[12%] -left-32 hidden lg:block"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -67,8 +97,12 @@ const scrollToSection = (section) => {
           d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z"
         />
       </svg>
-    </div>
-    <div class="absolute top-48 -right-52 hidden lg:block">
+    </motion.div>
+    <motion.div
+      :initial="{ x: -50, opacity: 0, delay: 5 }"
+      :whileInView="{ x: 0, opacity: 1, delay: 5 }"
+      class="absolute top-48 -right-52 hidden lg:block"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -79,8 +113,12 @@ const scrollToSection = (section) => {
           d="M2.273 5.625A4.483 4.483 0 0 1 5.25 4.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 3H5.25a3 3 0 0 0-2.977 2.625ZM2.273 8.625A4.483 4.483 0 0 1 5.25 7.5h13.5c1.141 0 2.183.425 2.977 1.125A3 3 0 0 0 18.75 6H5.25a3 3 0 0 0-2.977 2.625ZM5.25 9a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h13.5a3 3 0 0 0 3-3v-6a3 3 0 0 0-3-3H15a.75.75 0 0 0-.75.75 2.25 2.25 0 0 1-4.5 0A.75.75 0 0 0 9 9H5.25Z"
         />
       </svg>
-    </div>
-    <div class="absolute bottom-[20%] -right-44 -rotate-45 hidden lg:block">
+    </motion.div>
+    <motion.div
+      :initial="{ y: -50, opacity: 0, delay: 5 }"
+      :whileInView="{ y: 0, opacity: 1, delay: 5 }"
+      class="absolute bottom-[20%] -right-44 -rotate-45 hidden lg:block"
+    >
       <svg
         class="size-96"
         aria-hidden="true"
@@ -100,6 +138,6 @@ const scrollToSection = (section) => {
         />
         <path d="M10.5 14.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" />
       </svg>
-    </div>
+    </motion.div>
   </section>
 </template>
