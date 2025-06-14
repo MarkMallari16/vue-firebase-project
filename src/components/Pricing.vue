abfadadda89554236649fc05c20f5dd8ef15bdec
Mark Christian Mallari <mallariftw01@gmail.com>
2025-06-14 21:29:37 +0800

    upd: remove padding sides of social proof

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -38,7 +38,7 @@ const pricings = [
 </script>
 
 <template>
-  <section class="pb-40 px-10 lg:px-0 bg-gray-50">
+  <section class="pb-40  lg:px-0 bg-slate-100">
     <div class="text-center pt-20 pb-10">
       <h2 class="text-6xl font-extrabold">Choose your plan</h2>
       <p class="text-2xl text-gray-500 pt-6">
@@ -47,11 +47,8 @@ const pricings = [
     </div>
     <!-- Pricing Cards -->
     <div class="flex justify-center gap-8 flex-wrap pt-4">
-      <div
-        v-for="(pricing, index) in pricings"
-        class="p-10 ring-1 ring-inset ring-gray-300 w-96 bg-white rounded-lg"
-        :key="index"
-      >
+      <div v-for="(pricing, index) in pricings" class="p-10 ring-1 ring-inset ring-gray-300 w-96 bg-white rounded-lg"
+        :key="index">
         <div class="text-center">
           <h3 class="text-2xl font-bold">{{ pricing.title }}</h3>
           <h1 class="text-5xl font-medium mt-2 mb-4">
@@ -60,24 +57,11 @@ const pricings = [
           <p class="text-lg mb-8">{{ pricing.description }}</p>
         </div>
         <ul class="list-none pl-5 space-y-3">
-          <li
-            v-for="(feature, index) in pricing.features"
-            class="flex items-center gap-2"
-            :key="index"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-5 text-green-600"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
+          <li v-for="(feature, index) in pricing.features" class="flex items-center gap-2" :key="index">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-5 text-green-600">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
             <p class="text-gray-500">{{ feature }}</p>
           </li>
--- src/components/SocialProof.vue
+++ src/components/SocialProof.vue
@@ -2,14 +2,14 @@
 const socialProofs = [
   {
     name: "Kulas",
-    role: "Marketing Manager",
+    role: "Porman",
     testimonial:
       "BudgetTracker completely changed how I manage my finances. The insights are incredible and have helped me save over $500 per month.",
     image: "https://i.pinimg.com/474x/21/2a/cd/212acd3d51dc7661ed91c95e3b70d65c.jpg",
   },
   {
     name: "Gina Col",
-    role: "Software Engineer",
+    role: "Software Gingineer",
     testimonial:
       "I love how easy it is to track my expenses. The app is intuitive and has made budgeting a breeze.",
     image: "https://i.pinimg.com/736x/4d/13/13/4d13135ef1d77ed91ce80334ceab6ef5.jpg",
@@ -25,7 +25,7 @@ const socialProofs = [
 ];
 </script>
 <template>
-  <section class="py-20 px-16 lg:px-20 lg:py-32 lg:pb-48 w-full">
+  <section class="py-20 lg:py-32 lg:pb-48 w-full">
     <div class="text-center">
       <h1 class="text-6xl font-extrabold">Loved by thousands of users</h1>
       <p class="text-2xl pt-6 text-gray-500">
@@ -35,26 +35,15 @@ const socialProofs = [
     <div class="pt-16">
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-8">
         <!-- Loop through each social proof -->
-        <div
-          v-for="(proof, index) in socialProofs"
-          :key="index"
-          class="bg-white p-10 ring-1 ring-inset ring-base-300 rounded-lg shadow-md flex flex-col justify-between w-full"
-        >
+        <div v-for="(proof, index) in socialProofs" :key="index"
+          class="bg-white p-10 ring-1 ring-inset ring-base-300 rounded-lg shadow-md flex flex-col justify-between w-full">
           <div class="flex items-center">
             <!--Display Stars-->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-5"
-              v-for="(nums, index) in 5"
-              :key="index"
-            >
-              <path
-                fill-rule="evenodd"
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5"
+              v-for="index in 5" :key="index">
+              <path fill-rule="evenodd"
                 d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                clip-rule="evenodd"
-              />
+                clip-rule="evenodd" />
             </svg>
           </div>
           <div class="pt-4">
